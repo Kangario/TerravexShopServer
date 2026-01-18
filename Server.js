f6f2ad39ba78ca964b3058440ef327ddcabf28b8
Alexander Trushkov <mr.trushkoff2016@mail.ru>
2026-01-18 10:54:49 +0300

    Upd

--- Server.js
+++ Server.js
@@ -35,7 +35,7 @@ async function start() {
 
             const user = JSON.parse(rawUser);
             const now = Date.now();
-            const UPDATE_INTERVAL = 6000 * 100;
+            const UPDATE_INTERVAL = 6000 ;
             
             if (
                 user.lastShopUpdate &&
